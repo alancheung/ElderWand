93373903a60da2fec1034a7c4c04266c6b0dcabb
Alan <cheung.alan94@gmail.com>
2021-03-14 17:34:47 -0400

    PoC shape detection

--- ElderWand/ElderWand/Concept/shape.py
+++ ElderWand/ElderWand/Concept/shape.py
@@ -1,47 +1,72 @@
 import numpy as np
 import cv2
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img = cv2.imread('C:/Users/cheun/Desktop/CodeLibrary/ElderWand/ElderWand/ElderWand/WandTracker/Shapes/circle/shape1-1.png')
+canvas = np.zeros(img.shape, np.uint8)
 
-ret,thresh = cv2.threshold(gray,127,255,1)
-cv2.imshow('thresh', thresh)
+count = 0
 
-contours, h = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-cImage = cv2.drawContours(img, contours, -1, (255, 0, 0), 3)
-cv2.imshow('contours', cImage)
+n = 1
+while True:
+    img = cv2.imread('C:/Users/cheun/Desktop/CodeLibrary/ElderWand/ElderWand/ElderWand/WandTracker/Shapes/circle/shape1-' + str(n) + '.png')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+    ret,thresh = cv2.threshold(gray,127,255,1)
+    cv2.imshow('thresh', thresh)
 
-for cnt in contours:
-    area = cv2.contourArea(cnt)
-    x,y,w,h = cv2.boundingRect(cnt)
-    wip = img
-    cv2.rectangle(wip,(x,y),(x+w,y+h),(0,255,0),2)
+    contours, h = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = contours[1:]
+    cImage = cv2.drawContours(canvas, contours, -1, (255, 0, 0), 3)
+    cv2.imshow('contours', cImage)
 
-    shapeText = 'NO SHAPE'
+    lengths = []
+    found = False
+    for cnt in contours:
+        approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
 
-    approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
-    print(len(approx))
-    if len(approx) == 5:
-        shapeText = 'Pentagon'
-        cv2.drawContours(img,[cnt],0,255,1)
-    elif len(approx) == 3:
-        shapeText = 'Triangle'
-        cv2.drawContours(img,[cnt],0,(0,255,0),1)
-    elif len(approx) == 4:
-        shapeText = 'Square'
-        cv2.drawContours(img,[cnt],0,(0,0,255),1)
-    elif len(approx) == 9:
-        shapeText = 'Half-circle'
-        cv2.drawContours(img,[cnt],0,(255,255,0),1)
-    elif len(approx) > 15:
-        shapeText = 'Circle'
-        cv2.drawContours(img,[cnt],0,(0,255,255),1)
-        
-    if shapeText != 'NO SHAPE':
-        cv2.putText(wip, shapeText,(x,y-5), cv2.FONT_HERSHEY_SIMPLEX, .5,(255,255,255),1,cv2.LINE_AA)
-    cv2.imshow('WIP', wip)
-    cv2.waitKey(0)
+        approximationLength = len(approx)
+        lengths.append(approximationLength)
+        # print('Approximation of image ' + str(n) + ' has contour length of ' + str(approximationLength))
 
-cv2.imshow('img',img)
-cv2.waitKey(0)
+        shapeText = 'NO SHAPE'
+        if approximationLength == 5:
+            shapeText = 'Pentagon'
+        elif approximationLength == 3:
+            shapeText = 'Triangle'
+        elif approximationLength == 4:
+            shapeText = 'Square'
+        elif approximationLength == 9:
+            shapeText = 'Half-circle'
+        elif approximationLength >= 15:
+            shapeText = 'Circle'
+            
+        x,y,w,h = cv2.boundingRect(cnt)
+        if shapeText != 'NO SHAPE':
+            # print('Image ' + str(n) + ' found a shape of ' + shapeText + ' out of ' + str(len(contours[1:])) + ' contour shapes.')
+            color = (0, 255, 0)
+            cv2.putText(img, f'{shapeText} ({approximationLength})', (x,y-5), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+            count += 1
+            found = True
+            # break
+        else:
+            color = (0, 0, 255)
+            cv2.putText(img, str(approximationLength), (x,y-5), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+        cv2.rectangle(img, (x,y), (x+w,y+h), color, 1)
+
+    if found == False:
+        print('No shape, max lengths: ' + str(max(lengths)))
+
+    cv2.imshow('img',img)
+    key = cv2.waitKey(0)
+    if key == ord('b'):
+        n -= 1
+    elif key == ord('q'):
+        break
+    else:
+        n += 1
+
+    if n < 1 or n > 70:
+        break
+
 cv2.destroyAllWindows()
+print('Correct count: ' + str(count) + ' / 69')
--- ElderWand/ElderWand/ElderWand/elderWand.py
+++ ElderWand/ElderWand/ElderWand/elderWand.py
@@ -4,40 +4,69 @@ import cv2
 import imutils
 import time
 
-def detectSpell():
-    fullCnts = cv2.findContours(frame_gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-    return 'idk'
+def detectSpell(canvas):
+    spell = ''
+
+    contours, h = cv2.findContours(canvas, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = contours[1:]
+
+    for cnt in contours:
+        approx = cv2.approxPolyDP(cnt, 0.01*cv2.arcLength(cnt, True), True)
+        numSides = len(approx)
+        if numSides == 5:
+            spell = 'pentagon'
+            break
+        elif numSides == 3:
+            spell = 'triangle'
+            break
+        elif numSides == 4:
+            spell = 'square'
+            break
+        elif numSides == 9:
+            spell = 'half-circle'
+            break
+        elif numSides >= 15:
+            spell = 'circle'
+            break
+    return spell
 
 # loop over the set of tracked points so that we can draw a line for human eyes.
-def drawTrackingLine(frame_gray):
+def drawTrackingLine(canvas):
     for i in range(1, len(pts)):
         # if either of the tracked points are None, ignore them
         if pts[i - 1] is None or pts[i] is None:
             continue
 
         # otherwise, draw the connecting lines
-        cv2.line(frame_gray, pts[i - 1], pts[i], (255, 0, 0), thickness = 5)
+        cv2.line(canvas, pts[i - 1], pts[i], (255, 0, 0), thickness = 5)
+    # Then draw the last point to the first point in case they never finished the shape.    
+    cv2.line(canvas, pts[len(pts) - 1], pts[0], (0, 0, 255), thickness = 5)
 
 # The list of currently tracked points
-pts = deque(maxlen=64)
+pts = deque(maxlen=32)
 kernel = np.ones((5,5),np.uint8)
-thresh = 40
+thresh = 160
 
 # Initialize camera with 2s delay for camera to warm up
 camera = cv2.VideoCapture(0)
 time.sleep(2)
 
+imageSize = (720, 720)
+lastSpell = ''
+spellCount = 0
 while 1:
     # read from camera
     (grabbed, frame) = camera.read()
-    frame = imutils.rotate(frame, angle=180)
+    # frame = imutils.rotate(frame, angle=0)
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # TODO determine if this completely necessary
     cv2.equalizeHist(frame_gray)
 
     # resize image for faster processing.
-    frame_gray = cv2.resize(frame_gray, (120, 120), interpolation = cv2.INTER_CUBIC)
+    # frame_gray = cv2.resize(frame_gray, (120, 120), interpolation = cv2.INTER_CUBIC)
+    frame = cv2.resize(frame, imageSize)
+    frame_gray = cv2.resize(frame_gray, imageSize)
 
     # find the point by looking for pixels >threshold
     th, frame_gray = cv2.threshold(frame_gray, thresh, 255, cv2.THRESH_BINARY)
@@ -70,10 +99,11 @@ while 1:
 
     # TODO change the threshold of points to be configurable value??
     # detect likely spell if the number of tracked points is >=50%
-    drawTrackingLine(frame_gray)
+    canvas = np.zeros(imageSize, np.uint8)
+    drawTrackingLine(canvas)
     numPointsTracked = sum(1 for p in pts if p is not None)
-    if numPointsTracked >= (len(pts) / 2):
-        print('Shape is probable!' + str(numPointsTracked))
+    if numPointsTracked >= 16:
+        print('Shape is probable!')
 
         ## Get minimum image to verify
         #fullCnts = cv2.findContours(frame_gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
@@ -82,12 +112,27 @@ while 1:
         ## Debug, draw circle around image.
         #cv2.circle(frame_gray, (int(x), int(y)), int(radius), (0, 255, 255), 2)
 
-        spell = detectSpell()
-        print(spell)
+        spell = detectSpell(canvas)
+        if spell != '':
+            if spellCount >= 10:
+                print(f'Detected {spell} for last 32 frames!')
+                cv2.waitKey(0)
+                pts = deque(maxlen=32)
+                spellCount = 0
+                lastSpell = ''
+            elif spell == lastSpell:
+                spellCount += 1
+            else:
+                print(f'Last spell "{lastSpell}" detected for {spellCount} frames but detected new spell "{spell}"')
+                spellCount = 0
+                lastSpell = spell
+        else:
+            print('no spell detected in image')
 
     # loop and show frame
     cv2.imshow('raw', frame)
-    cv2.imshow('raw-grey', frame_gray)
+    cv2.imshow('grey', frame_gray)
+    cv2.imshow('points', canvas)
     keyPressed = cv2.waitKey(1) & 0xFF
     if keyPressed == ord('q'):
         break
